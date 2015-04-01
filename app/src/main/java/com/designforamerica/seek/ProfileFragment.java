3138fafc1b814240d309f3e5b9933c226d132421
Joe Bruzek <jbruzek@vt.edu>
2015-04-01 16:07:21 -0400

    finished setting up ProfileFragment for Simone

--- app/src/main/java/com/designforamerica/seek/ProfileFragment.java
+++ app/src/main/java/com/designforamerica/seek/ProfileFragment.java
@@ -6,15 +6,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import com.parse.ParseUser;
 
 /**
  * Created by jbruzek on 4/1/15.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements ParseCallbacks {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.profile_fragment, container, false);
+
+        /*
+        Get the current user. This is commented for now because we haven't implemented login
+        ParseUser user = ParseUser.getCurrentUser();
+        See the documentation about how to get information from the user
+        https://parse.com/docs/android_guide#users
+        */
 
         Bundle b = getArguments();
         TextView text = (TextView)v.findViewById(R.id.sample_textView);
@@ -23,4 +31,11 @@ public class ProfileFragment extends Fragment {
         return v;
     }
 
+    /**
+     * Called when the ParseHelper finishes a task
+     */
+    @Override
+    public void complete() {
+
+    }
 }
